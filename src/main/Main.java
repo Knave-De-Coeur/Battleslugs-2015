95fd347e0ec4e99a86e8ae81d1daae68cc7809b1
Knave-De-Coeur <alexanderm.1496@gmail.com>
2023-05-23 11:05:16 +0200

    Refactored code to use camel case variables and braces on same line.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,11 @@ package main;
 
 import javax.swing.JFrame;
 
-public class Main
-{
-	public static void main(String [] args)
-	{
+public class Main {
+	public static void main(String [] args) {
 		GUI game = new GUI();
-		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Helps the system not crash
-		game.setSize(1400,725);//Set the pixels
-		game.setVisible(true);//Shows the actual window
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.setSize(1400,725);
+		game.setVisible(true);
 	}
 }
--- src/main/Slugs.java
+++ src/main/Slugs.java
@@ -3,16 +3,14 @@ package main;
 import java.util.Random;
 import javax.swing.JOptionPane;
 
-public class Slugs
-{
-	private int slug_size = 5;//Required size of slug
-	private int slug_coordinates = 2;//X and Y
-	private int[][] slug = new int[slug_coordinates][slug_size];
-	private int x_co;
-	private int y_co;// variables
+public class Slugs {
+	private int slugSize = 5;//Required size of slug
+	private int slugCoordinates = 2;//X and Y
+	private int[][] slug = new int[slugCoordinates][slugSize];
+	private int xCo;
+	private int yCo;
 
-	public Slugs(int x, int y)
-	{
+	public Slugs(int x, int y) {
 		for(int size = 0; size < slug[1].length; size++)//Will iterate 5 times
 		{
 			for(int coordinate = 0; coordinate < slug.length; coordinate++)//Will iterate twice for 5 times
@@ -20,56 +18,46 @@ public class Slugs
 				slug[coordinate][size] = 0;// Nested loop to set all variables to 0 in multi dim array
 			}
 		}//Coordinates are generated from left to right each time
-		x_co = x;
-		y_co = y;
-	}//Constructor takes no arguments
+		xCo = x;
+		yCo = y;
+	}
 
-	public Slugs()
-	{
-		for(int size = 0; size < slug[1].length; size++)//Will iterate 5 times
-		{
-			for(int coordinate = 0; coordinate < slug.length; coordinate++)//Will iterate twice for five times
-			{
+	public Slugs() {
+		for(int size = 0; size < slug[1].length; size++) {
+			for(int coordinate = 0; coordinate < slug.length; coordinate++) {
 				slug[coordinate][size] = 0;// Nested loop to set all variables to 0 in multi dim array
 			}
 		}//Coordinates are generated from left to right each time
-		x_co = 0;
-		y_co = 0;
-	}//Constructor takes no arguments
-
-
-	public int getXco()
-	{
-		return x_co;
+		xCo = 0;
+		yCo = 0;
 	}
 
-	public int getYco()
-	{
-		return y_co;
+
+	public int getXco() {
+		return xCo;
 	}
 
-	public int getSlug(int x, int y)
-	{
-		try
-		{
+	public int getYco() {
+		return yCo;
+	}
+
+	public int getSlug(int x, int y) {
+		try {
 			return slug[x][y];//returns value of location
-		}catch(ArrayIndexOutOfBoundsException ex)//returns a value in a location that doesn't exist
-		{
-			JOptionPane.showMessageDialog(null,ex + "Caught, returning -1");
+		} catch (ArrayIndexOutOfBoundsException ex) {
+			JOptionPane.showMessageDialog(null, ex + "Caught, array out of bounds returning -1");
+			return -1;
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, ex + "Caught, returning -1");
 			return -1;
 		}
-		catch(Exception ex)// catches any other error
-		{
-			JOptionPane.showMessageDialog(null,ex + "Caught, returning -1");
-			return -1;
-		}//Always return a -1
-
 	}
-	//Accessors
 
-	public void Generate_SlugShape()// This will generate the random coordinates and store them in slug[][]
-	{
-		Random randomslug = new Random();
+	/**
+	 * This will generate the random coordinates and store them in slug[][]
+	 */
+	public void Generate_SlugShape() {
+		Random randomSlug = new Random();
 
 		int xpos = 0;
 		int ypos = 0;
@@ -83,77 +71,68 @@ public class Slugs
 		slug[1][0] = ypos;
 		//Centre of the slug which will be used as a place marker
 		boolean unique = false;// This boolean will determine when to stop the loop generating the coordinates
-		for(int i = 1; i < slug_size; i++)
-		{
-			do{
+		for(int i = 1; i < slugSize; i++) {
+			do {
 				int numb = 0;
-				numb = randomslug.nextInt(8)+1;// This gives the random generater the range of 1-8
-				switch(numb)
-				{
-					case 1:
-					{
-						xpos++;
-						ypos += 0;		/*Each case is dependent on the random number generated,
+				numb = randomSlug.nextInt(8)+1;// This gives the random generater the range of 1-8
+				/*Each case is dependent on the random number generated,
 										from that number the x and y coordinates will be altered
 										in one of 8 ways, that will set the coordinates around 0,0 and then
 										from the newly generated coordinates, new ones will be generated until,
 										the loop finishes all the while, constantly storing them in the multi dim
 										array */
-					}break;
+				switch (numb) {
+					case 1:
+						xpos++;
+						ypos += 0;
+						break;
 					case 2:
-					{
 						ypos--;
 						xpos++;
-					}break;
+						break;
+
 					case 3:
-					{
+					case 7:
 						xpos += 0;
 						ypos++;
-					}break;
+						break;
 					case 4:
-					{
 						xpos--;
 						ypos--;
-					}break;
+						break;
 					case 5:
-					{
 						xpos--;
 						ypos += 0;
-					}break;
+						break;
 					case 6:
-					{
 						ypos++;
 						xpos--;
-					}break;
-					case 7:
-					{
-						xpos += 0;
-						ypos++;
-					}break;
+						break;
 					case 8:
-					{
 						xpos++;
 						ypos++;
-					}
-				}/*After this line, unique is set true, a forloop is used to check each space in the array to see
+						break;
+					default:
+						break;
+				}
+				/*After this line, unique is set true, a forloop is used to check each space in the array to see
 				if there is the same pair of uniquely generated numbers, if so setting unique back true requiring the
 				do while to loop again until a unique set of numbers is generated*/
 				unique = true;
-				for(int a = 0; a < slug_size; a++)
-				{
-					if(xpos == slug[0][a] && ypos == slug[1][a])
-					{
+
+				for (int a = 0; a < slugSize; a++) {
+					if (xpos == slug[0][a] && ypos == slug[1][a]) {
 						unique = false;
 						xpos = 0;
 						ypos = 0;
+						break;
 					}
 				}// Validation of newly generated position
 
-			}while(unique == false);
+			} while(!unique);
 
 			slug[0][i] = xpos;
 			slug[1][i] = ypos; // once the uniquely generated numbers are generated and validated they are set in the array
 		}
 	}
-
 }
